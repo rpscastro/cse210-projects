061cb154565cc3f7c4b75b837ceaef7d6c539f95
rpscastro <rafael.p.s.castro@gmail.com>
2024-02-06 17:43:18 -0300

    W05 Prepare: Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+       
+        Square square = new Square("red", 2);
+
+        Console.WriteLine(square.GetColor());
+
+        Console.WriteLine(square.GetArea());
+
+
+        Rectangle rectangle = new Rectangle("blue",2,4);
+
+        Console.WriteLine(rectangle.GetColor());
+
+        Console.WriteLine(rectangle.GetArea());
+
+
+        Circle circle = new Circle("yellow", 4);
+
+        Console.WriteLine(circle.GetColor());
+
+        Console.WriteLine(circle.GetArea());
+
+
+        List<Shape> shapes =  new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes){
+
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
+
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,37 @@
+using System;
+
+public abstract class Shape
+{
+
+    public string _color;
+
+
+    public Shape(string color){
+
+        _color = color;
+    }
+
+
+
+    public string GetColor()
+    {
+
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+
+        _color = color;
+    }
+
+    // public virtual double GetArea(){
+
+    //     return 0;
+    // }
+
+    public abstract double GetArea();
+    
+
+
+}
